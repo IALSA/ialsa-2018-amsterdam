2d98cd9ba1fbe3a5851bf70f04424fb324486517
Andriy V. Koval <koval.andrey@gmail.com>
2018-01-06 17:28:43 -0800

    stable greeter

--- manipulation/map/0-greeter-map.R
+++ manipulation/map/0-greeter-map.R
@@ -30,27 +30,76 @@ requireNamespace("testit") # for asserting conditions meet expected patterns.
 # reach out to the curator for a dataset prepared for general consumption
 path_input  <- "../MAP/data-unshared/derived/dto.rds"
 # this is where the output of the this script will be stored:
-path_output <- "./data/unshared/derived/dto-0-raw.rds"
+path_output <- "./data/unshared/derived/dto-0-greeted.rds"
 
 
 # point to the local metadata to be used for this project (specific consumption)
 # metadata_path_input <- "./data/shared/meta/map/meta-data-map.csv" 
-metadata_path_input <- "./data/shared/meta/map/meta-data-map-2016-11-27.csv" 
+metadata_path_input <- "./data/shared/meta/map/meta-data-map-2017-12-28.csv" 
 
 # ---- load-data ------------------------------------------------
 # load data objects
 dto      <- readRDS(path_input)
 lapply(dto,names)
-# dto already contains meta data, but you should load a local one for greater control
-metaData <- read.csv(metadata_path_input, stringsAsFactors=F, header=T)
+# dto already contains meta data (a generic one), but you should develop/load a local one for greater control
+meta_data_local <- read.csv(metadata_path_input, stringsAsFactors=F, header=T)
+# How to create: you would start with the meta-data provided with the curator and make custom adjustment
 
 # ---- inspect-data ----------------------------------------------
 # inspect loaded data objects (using basic demographic variables )
 ds <- as.data.frame(dto$unitData) # assing alias to use in this session
 length(unique(ds$id))  # sample size, number of respondents
+ds %>% names()
 # t <- table(ds[,"fu_year"], ds[,"died"]); t[t==0]<-".";t 
 # t <- table(ds[,"msex"], ds[,"race"], useNA = "always"); t[t==0]<-".";t 
 # t <- table(ds[,"educ"], ds[,"race"]); t[t==0]<-".";t 
+
+
+
+# ---- chose-metadata -----------------------------------------------
+
+# meta_data <- dto$metaData # original, supplied by the ialsa-study-curator
+# alternatively, load a local meta-data file that adds on to the generic version from the curator
+meta_data <- meta_data_local
+
+## This chunk chooses the file to be used as project's meta-data file
+
+# ----- view-metadata-1 ---------------------------------------------
+# 
+meta_data %>% 
+  dplyr::arrange(type, construct, name) %>% 
+  dplyr::select(name, name_new, label, construct, longitudinal) %>% 
+  # head() %>% 
+  knitr::kable()
+
+
+# ----- apply-meta-data-1 -------------------------------------
+# create ds that would articulate the renaming rules to be applied
+ds_renamings <- 
+  dplyr::left_join(
+    # 1st object
+    tibble::tibble(
+      "name_raw" = ds %>% names()
+    ),
+    # 2nd object
+    meta_data %>% 
+      dplyr::select(name, name_new),
+    # by clause
+    by = c("name_raw"="name")
+    ) %>% 
+  # additional tranformations
+  dplyr::mutate(
+    # create values for variable names in the future data set
+    renamed = ifelse(is.na(name_new),name_raw,name_new)
+  )
+names(ds) <- ds_renamings$renamed
+ds %>% dplyr::glimpse()
+
+# transfer changes to dto
+ds <- ds %>% dplyr::filter(study == "MAP ") # in case we have multiple studies (not really relevant here)
+table(ds$study) # should be only one study
+dto[["unitData"]] <- ds
+
 
 # ----- tweak-data -----------------------
 # apply general tweaks on  unit data
@@ -62,30 +111,10 @@ dto <- list()
 # the first element of data transfer object contains unit data
 dto[["unitData"]] <- ds
 # the second element of data transfer object contains meta data
-dto[["metaData"]] <-  metaData # new, local meta-data!!
+dto[["metaData"]] <-  meta_data # new, local meta-data!!, disable if using original metadata
 # verify and glimpse
 # dto[["unitData"]] %>% dplyr::glimpse()
 # dto[["metaData"]] %>% dplyr::glimpse()
-
-
-# ----- view-metadata-1 ---------------------------------------------
-meta_data <- dto[["metaData"]] %>%
-  dplyr::filter(include == TRUE) %>%
-  # dplyr::select(name, name_new, type, label, construct) %>%
-  dplyr::arrange(type, construct, name) %>% 
-  dplyr::select(name, name_new, label, construct, longitudinal)
-knitr::kable(meta_data)
-
-# # ----- apply-meta-data-1 -------------------------------------
-# # rename variables
-# d_rules <- dto[["metaData"]] %>%
-#   dplyr::filter(name %in% names(ds)) %>% 
-#   dplyr::select(name, name_new ) # leave only collumn, which values you wish to append
-# names(ds) <- d_rules[,"name_new"]
-# # transfer changes to dto
-# ds <- ds %>% dplyr::filter(study == "MAP ")
-# table(ds$study)
-# dto[["unitData"]] <- ds 
 
 # ---- save-to-disk ------------------------------------------------------------
 
